b2e110f416fa88324078c6f0e9db63a30857d9ef
Slava206 <slava.tepaev@yandex.ru>
2025-01-09 21:05:30 +0300

    Add files via upload

---
+++ Лаб. 2/Лаба №2, Задание№1.py
@@ -0,0 +1,20 @@
+def greet(name):
+    print(f"Привет, {name}!")
+greet("Андрей")
+
+'''
+def square(number):
+    return number ** 2
+result = square(5)
+print(result)
+'''
+
+'''
+def max_of_two(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+max = max_of_two(5, 2)
+print(max)
+'''
---
+++ Лаб. 2/Лаба №2, Задание№2.py
@@ -0,0 +1,18 @@
+def describe_person(name, age=30):
+    print(f"Меня зовут {name}, мне {age} лет.")
+describe_person("Дима")
+
+'''
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+print(is_prime(2))
+print(is_prime(3))
+print(is_prime(4))
+print(is_prime(17))
+print(is_prime(18))
+'''
